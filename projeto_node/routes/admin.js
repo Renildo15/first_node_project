15d14be6a76528abe2ca8ab6837781759aa3e48d
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-02-06 19:25:46 -0300

    adicionando edit

--- projeto_node/routes/admin.js
+++ projeto_node/routes/admin.js
@@ -63,4 +63,34 @@ router.post("/categorias/nova",(req, res)=>{
     
 })
 
+router.get("/categorias/edit/:id", (req, res)=>{
+    Categoria.findOne({_id:req.params.id}).lean().then((categoria)=>{
+        res.render("admin/edit_categorias", {categoria:categoria});
+    }).catch((err)=>{
+        req.flash("error_msg","Essa categoria não existe!");
+        res.redirect("/admin/categorias");
+    })
+})
+
+router.post("/categorias/edit", (req, res)=>{
+
+    Categoria.findOne({_id:req.body.id}).then((categoria)=>{
+        
+        categoria.nome = req.body.nome;
+        categoria.slug = req.body.slug
+
+        categoria.save().then(()=>{
+            req.flash("success_msg","Categoria editada com sucesso!")
+            res.redirect("/admin/categorias")
+        }).catch((err)=>{
+            req.flash("error_msg","Houve um erro interno ao salvar a categoria!");
+            res.redirect("/admin/categorias");
+        })
+    }).catch((err)=>{
+        req.flash("error_msg","Houve um erro ao editar a categoria!")
+        res.redirect("/admin/categorias");
+        console.log(err)
+    })
+});
+
 module.exports = router
