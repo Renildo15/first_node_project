74417fd14c2d2f3c2fe846113186be774b2444c0
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-02-07 19:06:21 -0300

    salvando no banco de dados

--- projeto_node/routes/admin.js
+++ projeto_node/routes/admin.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require("../models/Categoria")
 const Categoria = mongoose.model("categorias")
-const bodyParser = require('body-parser');
+require("../models/Postagem")
+const Postagem = mongoose.model("postagens")
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
@@ -121,7 +123,7 @@ router.post("/categorias/deletar",(req, res)=>{
 })
 
 router.get("/postagens", (req,res)=>{
-    res.render("admin/postagens",{categorias:categorias})
+    res.render("admin/postagens")
 })
 
 
@@ -134,5 +136,33 @@ router.get("/postagens/add", (req, res)=>{
         console.log(err)
     })
     
+})
+
+router.post("/postagens/nova",(req,res)=>{
+    var errors = []
+
+    if(req.body.categoria == "0"){
+        errors.push({texto: "Categoria inválida, registre uma categoria"})
+    }
+
+    if(errors.length > 0){
+        res.redirect("admin/add_postagens", {errors:errors})
+    }else{
+        const novaPostagens = {
+            titulo: req.body.titulo,
+            descricao: req.body.descricao,
+            conteudo: req.body.categoria,
+            categoria: req.body.categoria,
+            slug: req.body.slug
+        }
+
+        new Postagem(novaPostagens).save().then(()=>{
+            req.flash("success_msg","Postagem criada com sucesso!")
+            res.redirect("/admin/postagens")
+        }).catch((err)=>{
+            req.flash("error_msg","Houve um erro durante o salvamento da postagem")
+            res.redirect("/admin/postagens")
+        })
+    }
 })
 module.exports = router
