8844010dbbc5b30bb2dc5dcc5eb7c0db425ee35c
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-02-07 18:45:11 -0300

    adicionando formulario

--- projeto_node/routes/admin.js
+++ projeto_node/routes/admin.js
@@ -120,4 +120,19 @@ router.post("/categorias/deletar",(req, res)=>{
     })
 })
 
+router.get("/postagens", (req,res)=>{
+    res.render("admin/postagens",{categorias:categorias})
+})
+
+
+router.get("/postagens/add", (req, res)=>{
+    Categoria.find().lean().then((categorias)=>{
+        res.render("admin/add_postagens", {categorias:categorias})
+    }).catch((err)=>{
+        req.flash("error_msg","Houve um erro ao carregar formulário!")
+        res.redirect("/admin");
+        console.log(err)
+    })
+    
+})
 module.exports = router
