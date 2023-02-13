e286874cd440faa7e37b777e16270632c0359bba
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-02-13 14:41:03 -0300

    listando posts

--- projeto_node/app.js
+++ projeto_node/app.js
@@ -11,7 +11,9 @@
     const moment = require('moment');
     const router = require('./routes/admin');
     require("./models/Postagem")
+    require("./models/Categoria")
     const Postagem = mongoose.model("postagens")
+    const Categoria = mongoose.model("categorias")
 
 // Configurações
 
@@ -85,6 +87,35 @@
             res.redirect("/")
         })
     })
+
+    app.get("/categorias",(req, res)=>{
+        Categoria.find().lean().then((categorias)=>{
+            res.render("categorias/index",{categorias:categorias})
+        }).catch((err)=>{
+            req.flash("error_msg","Houve um erro interno!")
+            res.redirect("/")
+        })
+    })
+
+    app.get("/categorias/:slug",(req,res)=>{
+        Categoria.findOne({slug:req.params.slug}).lean().then((categoria)=>{
+            if(categoria){
+                Postagem.find({categoria:categoria._id}).lean().then((postagens)=>{
+                    res.render("categorias/postagens", {postagens:postagens, categoria:categoria})
+                }).catch((err)=>{
+                    req.flash("error_msg","Houve um erro interno!")
+                    res.redirect("/")
+                })
+            }else{
+                req.flash("error_msg","Essa categoria não existe!")
+                res.redirect("/")
+            }
+        }).catch((err)=>{
+            req.flash("error_msg","Houve um erro interno!")
+            res.redirect("/")
+        })
+    })
+
     app.get("/404",(req, res)=>{
         res.send("Erro 404!")
     })
